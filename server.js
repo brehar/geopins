bf592b0ecd2f4ab63344cfa828620ed65d2c7115
Brett Hartman <brett.hartman@mac.com>
2019-04-03 00:08:03 -0700

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+mongoose
+	.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(() => console.log('DB connected!'))
+	.catch(err => console.error(err));
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
